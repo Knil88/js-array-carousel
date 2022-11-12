d9f84f2432a90469c70035104445261b949f0ee8
Yari <narducciyarigmail.com>
2022-11-12 19:35:22 +0100

    Agggiunta commenti

--- js/script.js
+++ js/script.js
@@ -20,45 +20,68 @@ imgList[4].src = 'img/05.webp';
 
 
 
-//Creiamo il ciclo
+//Creiamo il ciclo 
 
 for(let i=0 ; i < imgList.length; i++){
     
     let cont = document.getElementById("items-container");
     let div = document.createElement("div");
     
+    //Settiamo le dimensioni delle immagini
+
     div.style.width = '800px';
     div.style.height = '500px';
+
+    //Appendiamo dentro il div la lista immagini
+
     div.append(imgList[i]);
+
+    //Appendiamo dentro il item-container(ovvero la variabile cont) il div
+
     cont.append(div);
-    
+
+    //Aggiungiamo la classe img ed item che darà diplay none ai contenitori
     div.classList.add("img");
     div.classList.add("item");
 
-   
-    
-      
-      
-     
-    console.log(imgList[i]);
-
-
-    
-
+   console.log(imgList[i]);
 
 }
 
+
+//Creiamo la costante divArray prendendo agganciandola ad img
+
+
 const divArray = document.getElementsByClassName("img");
+
+//Settiamo la costante in modo che il primo elemento presente nell'array abbia calsse active che corrisponde a display block
+
 divArray[0].classList.add("active");
+
+//Creiamo la variante activeItem e gli diciamo che ha valore 0
+
 let activeItem = 0 ; 
 
 // Creiamo evento bottone 
 
 btnDown.addEventListener("click",
     function(){
+
+        //Poniamo la condizione che se la constante activeItem è minore della lunghezza della lista divArray
+
         if(activeItem  < divArray.length - 1) {
+
+            //ogni volta che clicchiamo al div precendente viene tolta la classe active
+
+
             divArray[activeItem].classList.remove("active");
+
+            //Di conseguenza il numero dell'elemento in lista aumenta passando da 0 ad 1 e cosi via .
+
             activeItem++;
+
+            //Cosi il div successivo gli viene aggiunta la classe active in modo che l'immagine possa apparire
+            
             divArray[activeItem].classList.add("active");
 
         }
